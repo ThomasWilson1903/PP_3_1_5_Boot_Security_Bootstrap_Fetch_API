4de1ef29673e42cad08a4062238929fbf25b5b30
AleksandrBirukov <hdnhdxv@gmail.com>
2023-11-16 18:57:29 +0400

    add AdminControllerREST edit WebSecurityConfig, first row js

---
+++ src/main/resources/templates/js/index.js
@@ -0,0 +1,34 @@
+const url = 'http://localhost:8080/rest/users'
+let jsonData
+let table = document.getElementById("table");
+var tableHeader = table.createTHead();
+var headerRow = tableHeader.insertRow();
+headerRow.insertCell().textContent = "username";
+headerRow.insertCell().textContent = "firstname";
+headerRow.insertCell().textContent = "doblename";
+
+const dsa = document.getElementById('res');
+fetch('http://localhost:8080/rest/users')
+    .then(response => response.json())
+    .then(data => {
+        // Обработка данных
+        console.log(data)
+        dsa.textContent = data
+        jsonData = data;
+
+        // Заполнение таблицы данными из JSON
+        var tableBody = table.createTBody();
+        jsonData.forEach(function (item) {
+            var row = tableBody.insertRow();
+            row.insertCell().textContent = item.username;
+            row.insertCell().textContent = item.firstName;
+            row.insertCell().textContent = item.lastName;
+        });
+    })
+    .catch(error => {
+        // Обработка ошибок
+        console.error('Ошибка:', error);
+    });
+
+
+
